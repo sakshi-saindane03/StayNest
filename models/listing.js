d23979574f9a00036979fc798a91a3f46f206ebe
sakshi saindane <sakshisaindane03@gmail.com>
2026-08-17 14:02:04 +0530

    Create listing schema and model

---
+++ models/listing.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+    },
+    image:{
+        type: String,
+        set: (v) => v === ""? "https://unsplash.com/photos/wooden-sauna-interior-with-forest-view-xrjx6nX3Mp4https://unsplash.com/photos/milky-way-above-radio-tower-vf_nxn3UcAA" : v,
+    },
+    price:{
+        type: Number
+    },
+    location:{
+        type: String,
+    },
+    country:{
+        type: String,
+    },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.export = Listing;
