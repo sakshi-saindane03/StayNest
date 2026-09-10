5edb4597ce90f89cc8422a7a6f5cc6edc1cfa154
sakshi saindane <sakshisaindane03@gmail.com>
2026-09-10 22:52:28 +0530

    Add reviews functionality

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -23,6 +24,18 @@ const listingSchema = new Schema({
     country:{
         type: String,
     },
+    reviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Review"
+        }
+    ]
+});
+
+listingSchema.post("findOneAndDelete", async(listing) => {
+    if(listing) {
+        await Review.deleteMany({_id: { $in: listing.reviews }});
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
